c7e193d208406e4a4e9054caa0c230e6d6a20f9f
Shihab Islam <Shihab.Islam90@myhunter.cuny.edu>
2021-10-04 21:14:38 -0400

    useless array line initialized that is not needed.

--- myEach.js
+++ myEach.js
@@ -1,6 +1,5 @@
 //Foreach
 // without "Array.prototype.foreach" we can mimic the effect by executing a callback function for all elements in a array.
-const arr = [10, 20, 30]
 
 Array.prototype.myEach = function (callbackFn) {
     for (let i = 0; i < this.length; i++) {
